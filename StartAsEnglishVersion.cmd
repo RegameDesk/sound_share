238507a5b903e5141d3d18fd25805ec63d3a97da
UMU618 <umu618@hotmail.com>
2025-02-01 23:02:16 +0800

    * Fix crash of Boost 1.86
    https://github.com/boostorg/beast/issues/2941
    
    + Audio Bitrate

--- StartAsEnglishVersion.cmd
+++ StartAsEnglishVersion.cmd
@@ -1,4 +1,4 @@
 @echo off
-start "SoundShare" "%~dp0bin\SoundShare.exe" regame://SoundShare?lang=en-US
+start "SoundShare" "%~dp0bin\SoundShare.exe" regame://SoundShare?lang=en-US&audio_bitrate=128000&mute=once
 REM start "SoundShare" "%~dp0bin\SoundShare.exe" regame://SoundShare?lang=en-US&mute=once
 REM start "SoundShare" "%~dp0bin\SoundShare.exe" regame://SoundShare?lang=en-US&mute=keep
